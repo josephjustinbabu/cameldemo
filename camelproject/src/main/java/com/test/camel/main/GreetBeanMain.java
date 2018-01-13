3950ed305504b4c8e9da10d55eca48e25504578b
joseph <josephjustinbabu@gmail.com>
2018-01-13 12:54:32 +0530

    t

--- camelproject/src/main/java/com/test/camel/main/GreetBeanMain.java
+++ camelproject/src/main/java/com/test/camel/main/GreetBeanMain.java
@@ -20,7 +20,7 @@ public class GreetBeanMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ApplicationContext context = null;
-		String name = null;
+		String name = "Judit";
 		try{
 			context = new ClassPathXmlApplicationContext("applicationContext.xml");
 			GreetMeBean greeter = (GreetMeBean)context.getBean("greetMeBean");
